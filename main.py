b3ac867b3e7921804b95db34659c012b1c9b9e02
martynasp3 <martynasp1313@gmail.com>
2025-05-31 23:03:09 +0100

    added code to clean and transform data

--- main.py
+++ main.py
@@ -36,7 +36,21 @@ if os.path.exists(filename):
     old_df = pd.read_csv(filename)
     df = pd.concat([old_df, df], ignore_index=True)
 
+# removed whitespace, standardised casing and replaced empty strings with null
+df["company"] = df["company"].str.strip().str.title().replace("", pd.NA)
+df["position"] = df["position"].str.strip().str.title().replace("", pd.NA)
+df["location"] = df["location"].str.strip().replace("", pd.NA)
+
+# splits tags into separate strings
+df["tags"] = df["tags"].fillna("").apply(lambda x: [tag.strip().lower() for tag in x.split(",") if tag])
+
 # removing dupes
 df.drop_duplicates(subset=["company","position","salary_avg"], inplace=True)
+
+# removing rows that are missing company or position data
+df.dropna(subset=["company","position"], inplace=True)
+
+df["is_dev_job"] = df["tags"].apply(lambda tags: any("dev" in tag for tag in tags))
+df["is_data_job"] = df["tags"].apply(lambda tags: any("data" in tag for tag in tags))
 
 df.to_csv("jobs.csv", index=False)
